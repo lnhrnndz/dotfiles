672615b31134dab12fe2a4ffae9b5de888d0737d
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-05-10 20:04:57 +0200

    Add support for multiple batteries
    * dwm-status and tmux-battery scripts show battery levels side by side

--- scripts/bin/tmux-battery.sh
+++ scripts/bin/tmux-battery.sh
@@ -1,33 +1,27 @@
 #!/bin/sh
 # script to add battery status to tmux status bar
 
-if [ -e /sys/class/power_supply/BAT0/capacity ]; then
-    battery="$(cat /sys/class/power_supply/BAT0/capacity)"
-    batterystatus="$(cat /sys/class/power_supply/BAT0/status)"
-elif [ -e /sys/class/power_supply/BAT1/capacity ]; then
-    battery="$(cat /sys/class/power_supply/BAT1/capacity)"
-    batterystatus="$(cat /sys/class/power_supply/BAT1/status)"
-else
-    exit 1
-fi
-
-charging="#[bg=colour119,fg=colour000]"
-normal="#[bg=colour40,fg=colour000]"
+charging="#[bg=colour40,fg=colour000]"
 low="#[bg=colour214,fg=colour234]"
 critical="#[bg=colour160,fg=colour255]"
 
+for BATTERY in /sys/class/power_supply/BAT* ; do
 
-if [ "$batterystatus" = Charging ] && [ "$battery" -lt 99 ]; then
-    echo "$charging $battery% "
-    exit 0
-fi
+    battery="$(cat ${BATTERY}/capacity)"
+    batterystatus="$(cat ${BATTERY}/status)"
 
-if [ "$(( battery > 20 ))" = 1 ]; then
-    echo "$battery% "
-elif [ "$(( battery > 5 ))" = 1 ]; then
-    echo "$battery% "
-else
-    echo "$critical !!!!! $battery% !!!!! "
-fi
+    if [ "$batterystatus" = Charging ] && [ "$battery" -lt 99 ]; then
+        printf "%s %s%% " "$charging" "$battery"
+        exit 0
+    fi
 
-exit 0
+    if [ "$(( battery > 20 ))" = 1 ]; then
+        printf " %s%% " "$battery"
+    elif [ "$(( battery > 5 ))" = 1 ]; then
+        printf "%s %s%% " "$low" "$battery"
+    else
+        printf "%s !!!!! %s%% !!!!! " "$critical" "$battery"
+    fi
+done
+
+printf "\\n"
