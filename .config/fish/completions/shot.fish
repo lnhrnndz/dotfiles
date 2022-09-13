814b17506de74fdfdf5d0143e94286a093775e0f
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-09-14 01:39:47 +0200

    Add shot script
    screenshot script written in fish

---
+++ .config/fish/completions/shot.fish
@@ -0,0 +1,6 @@
+complete -c shot -f
+complete -c shot -s h -l help -d "Display help"
+complete -c shot -s n -l name -d "Use NAME as filename"
+complete -c shot -s a -l all -d "Screenshot all available monitors"
+complete -c shot -s f -l focused -d "Screenshot focused window"
+complete -c shot -s s -l select -d "Visually select area"
