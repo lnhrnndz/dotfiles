13dca9b46158f359a67f6cc24b84155df1a7e22f
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-10-19 11:05:03 +0200

    Update fish

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -18,7 +18,16 @@ abbr -a -- lal 'ls -al'
 abbr -a -- lt 'exa --sort newest -l'
 # alias ls in fish/functions
 
+# fzf #
+abbr -a -- fcd 'fzf-cd-widget'
+abbr -a -- fvim 'vim $(find . | fzf)'
+
 abbr -a -- sxhkdrc 'nvim ~/.config/sxhkd/sxhkdrc'
 
+# tree #
 abbr -a -- treee 'tree --dirsfirst -F'
 abbr -a -- dotfilestree 'tree --dirsfirst -F -a . -I .git'
+bind -M insert -- \et 'tree --dirsfirst -F'
+
+# WSL #
+abbr -a -- e 'explorer.exe'
---
+++ .config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/leon/packages/fzf/shell/key-bindings.fish
