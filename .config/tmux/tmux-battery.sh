efb7ee278e7c7d897681348ee4d83c0c2bb8cfb6
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2023-05-21 15:59:53 +0200

    spaces -> tabs

--- .config/tmux/tmux-battery.sh
+++ .config/tmux/tmux-battery.sh
@@ -7,21 +7,21 @@ critical="#[bg=colour160,fg=colour255]"
 
 for BATTERY in /sys/class/power_supply/BAT* ; do
 
-   battery="$(cat "${BATTERY}"/capacity)"
-    batterystatus="$(cat "${BATTERY}"/status)"
+	battery="$(cat "${BATTERY}"/capacity)"
+	batterystatus="$(cat "${BATTERY}"/status)"
 
-    if [ "$batterystatus" = Charging ] && [ "$battery" -lt 99 ]; then
-        printf "%s %s%% " "$charging" "$battery"
-        exit 0
-    fi
+	if [ "$batterystatus" = Charging ] && [ "$battery" -lt 99 ]; then
+		printf "%s %s%% " "$charging" "$battery"
+		exit 0
+	fi
 
-    if [ "$(( battery > 20 ))" = 1 ]; then
-        printf " %s%% " "$battery"
-    elif [ "$(( battery > 5 ))" = 1 ]; then
-        printf "%s %s%% " "$low" "$battery"
-    else
-        printf "%s !!!!! %s%% !!!!! " "$critical" "$battery"
-    fi
+	if [ "$(( battery > 20 ))" = 1 ]; then
+		printf " %s%% " "$battery"
+	elif [ "$(( battery > 5 ))" = 1 ]; then
+		printf "%s %s%% " "$low" "$battery"
+	else
+		printf "%s !!!!! %s%% !!!!! " "$critical" "$battery"
+	fi
 done
 
 printf "\\n"
