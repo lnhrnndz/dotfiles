93d534c243c1dff17f839e6789f26e4155c3bbf9
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2024-07-05 14:46:50 +0200

    Change prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,39 +13,39 @@ end
 export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_LAUNCHD_SESSION_BUS_SOCKET"
 
 
-function fish_mode_prompt
-  set_color brblue
-  fish_vcs_prompt > /dev/null
-  and printf "%s " (fish_vcs_prompt | sed 's/^ //')
-  set_color normal
-  switch $fish_bind_mode
-    case default
-      set_color brred
-    case insert
-      set_color brgreen
-    case replace
-      set_color bryellow
-    case replace_one
-      set_color bryellow
-    case visual
-      set_color brmagenta
-    case '*'
-      set_color brwhite
-  end
+#function fish_mode_prompt
+#  set_color brblue
+#  fish_vcs_prompt > /dev/null
+#  and printf "%s " (fish_vcs_prompt | sed 's/^ //')
+#  set_color normal
+#  switch $fish_bind_mode
+#    case default
+#      set_color brred
+#    case insert
+#      set_color brgreen
+#    case replace
+#      set_color bryellow
+#    case replace_one
+#      set_color bryellow
+#    case visual
+#      set_color brmagenta
+#    case '*'
+#      set_color brwhite
+#  end
+#  printf '%s' (prompt_pwd)
+#  set_color normal
+#  printf '>> '
+#end
+
+function fish_prompt
+  set_color magenta
+  printf '%s@%s' $USER $hostname
+  printf '%s ' (fish_vcs_prompt)
+  set_color green
   printf '%s' (prompt_pwd)
   set_color normal
   printf '> '
-end
-
-function fish_prompt
-  #set_color magenta
-  ##printf '%s@%s' $USER $hostname
-  ##printf '%s ' (fish_vcs_prompt)
-  #set_color green
-  #printf '%s' (prompt_pwd)
-  #set_color normal
-  #printf '> '
-  #set_color normal
+  set_color normal
 end
 
 function fish_right_prompt
